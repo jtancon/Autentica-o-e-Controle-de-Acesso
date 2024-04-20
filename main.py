89009b91024e69dee540a6cba68b1d8f404e1df5
jtancon <101944685+jtancon@users.noreply.github.com>
2024-04-20 14:56:41 -0300

    while criar

--- main.py
+++ main.py
@@ -20,6 +20,14 @@ def verificar_permissao(permissoes, username, acao, recurso):
             return True
     return False
 
+def criar_arquivo(permissoes, username, acao, recurso):
+    if username in permissoes:
+        if acao in permissoes[username] and recurso in permissoes[username][acao]:
+            with open(recurso, 'w') as f:
+                f.write('')
+            return True
+    return False
+
 def main():
     usuarios = carregar_usuarios()
     permissoes = carregar_permissoes()
@@ -28,23 +36,38 @@ def main():
     print("Bem Vindo ao nosso Sistema de Permissões")
     print("========================================")
 
+    while True:
+        username = input("Digite seu nome de usuário : ")
+        password = input("Digite sua senha: ")
 
-    username = input("Digite seu nome de usuário: ")
-    password = input("Digite sua senha: ")
+        if autenticar_usuario(usuarios, username, password):
+            print("Autenticação bem-sucedida! Bem vindo", username)
+            break
+        else:
+            print("Usuário ou senha inválidos.")
+        sair = input("Deseja sair? (s/n): ")
+        if sair.lower() == 's' or sair.lower() == 'sair':
+            return
 
-    if autenticar_usuario(usuarios, username, password):
-        print("Autenticação bem-sucedida! Bem vindo", username)
-    else:
-        print("Usuário ou senha inválidos.")
-        return
+    while True:
+        acao = input("Digite a ação desejada (ler, escrever, apagar, criar): ")
+        recurso = input("Digite o nome do recurso: ")
 
-    acao = input("Digite a ação desejada (ler, escrever, apagar): ")
-    recurso = input("Digite o nome do recurso: ")
+        if acao == 'criar':
+            if criar_arquivo(permissoes, username, acao, recurso):
+                print("Arquivo criado com sucesso.")
+            else:
+                print("Você não tem permissão para criar arquivos.")
+        else:
+            if verificar_permissao(permissoes, username, acao, recurso):
+                print("Acesso permitido.")
+            else:
+                print("Acesso negado.")
 
-    if verificar_permissao(permissoes, username, acao, recurso):
-        print("Acesso permitido.")
-    else:
-        print("Acesso negado.")
+        print()
+        sair = input("Deseja sair? (s/n): ")
+        if sair.lower() == 's' or sair.lower() == 'sair':
+            break
 
 if __name__ == "__main__":
     main()
